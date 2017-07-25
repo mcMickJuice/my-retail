a8ca0a562c3bbe3c7a2f3f80712a6db21c6a3f32
Mike Joyce <mikejoyce19@gmail.com>
2017-07-24 20:20:33 -0500

    added cross-env. Added production setting to webpack.prod

--- build/webpack.prod.js
+++ build/webpack.prod.js
@@ -1,9 +1,17 @@
 const webpackBase = require('./webpack.base')
 const merge = require('webpack-merge')
 const Uglify = require('uglifyjs-webpack-plugin')
+const webpack = require('webpack')
 
 const webpackProd = {
-  plugins: [new Uglify()]
+  plugins: [
+    new webpack.DefinePlugin({
+      'process.env': {
+        NODE_ENV: JSON.stringify('production')
+      }
+    }),
+    new Uglify()
+  ]
 }
 
 module.exports = merge(webpackBase, webpackProd)
